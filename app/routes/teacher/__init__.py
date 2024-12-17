d0624d231f1b8e8a83427ddf180122001339cfa9
unoAAz <jamesfurankrin@gmail.com>
2024-12-17 14:54:13 +0900

    [delete]index.py削除2

--- app/routes/teacher/__init__.py
+++ app/routes/teacher/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 
-from .index import index_bp
 from .register import register_bp
 from .login import login_bp
 from .dashboard import dashboard_bp
@@ -10,7 +9,6 @@ from .lecture import lecture_bp
 teacher_bp = Blueprint('teacher', __name__)
 
 # 各Blueprintを登録
-teacher_bp.register_blueprint(index_bp)
 teacher_bp.register_blueprint(register_bp)
 teacher_bp.register_blueprint(login_bp)
 teacher_bp.register_blueprint(dashboard_bp)
